2deeaf694fdb4edef95a77e2b589e37cd080b9b6
ahnjeonghyun <tgrf3090@gmail.com>
2021-04-29 20:26:05 +0900

    [19기 안정현]Refator: 멘토 리뷰 반영

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import my_settings
 
-# Create your views here.
+from django.http     import JsonResponse
+from django.views    import View
+
+from products.models import MainCategory, SubCategory
+
+class CategoryListView(View):
+    def get(self,request):
+        categories = MainCategory.objects.all()
+        
+        result = [{
+            'category_id'      : category.id,
+            'category_name'    : category.name,
+            'subcategories'    : [{
+                'id'               : subcategory.id,
+                'subcategory_name' : subcategory.name } for subcategory in category.subcategory_set.all()]}
+                for category in categories ]
+                 
+
+        return JsonResponse({'result':result}, status=200) 
