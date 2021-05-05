06a910405dc9d6a0ce9cc74c598f064c9c5e5fa9
hyun0k <hyunyoung9310@gmail.com>
2021-05-05 12:33:45 +0900

    Add:프로필 등록 엔드포인트 구현 - 프로필 사진 및 닉네임 등록하는 기능 - 프로필 사진은 S3에 업로드하고 DB 업데이트 - Json 메시지 키값 소문자로 수정 - key error 발생하도록 get함수 대신 키값으로 바로 접근하도록 수정

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 import requests
 import json
 import jwt
+import boto3
 
 from django.http  import JsonResponse
 from django.views import View
 
 from .models      import User, SocialPlatform
-from my_settings  import SECRET_KEY, ALGORITHM
+from .utils       import login_decorator
+from my_settings  import SECRET_KEY, ALGORITHM, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME, DEFAULT_IMAGE_URL
 
 class KakaoSignInView(View):
     def post(self, request):
@@ -28,11 +30,63 @@ class KakaoSignInView(View):
                 
             return JsonResponse(
                     {
-                        'MESSAGE'      : 'SUCCESS', 
-                        'ACCESS_TOKEN' : access_token,
-                        'IS_NEW'       : created
+                        'message'      : 'SUCCESS', 
+                        'access_token' : access_token,
+                        'is_new'       : created
                     }, status=200
                 )
             
         except KeyError:
-            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400) 
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400) 
+
+
+class ProfileRegisterView(View):
+    @login_decorator
+    def post(self, request):
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        )
+        try:
+            user       = request.user
+            nickname   = request.POST['json']
+            image_file = request.FILES['file']
+            
+            key      = str(user.user_code)
+            location = s3_client.get_bucket_location(Bucket=AWS_S3_BUCKET_NAME)['LocationConstraint']
+                                
+            s3_client.upload_fileobj(
+                                image_file, 
+                                AWS_S3_BUCKET_NAME,
+                                f'user-profile-image/{key}/{image_file.name}',
+                                ExtraArgs={
+                                    "ContentType": image_file.content_type
+                                }
+                            )
+            
+            url      = f'http://{AWS_S3_BUCKET_NAME}.s3.{location}.amazonaws.com/user-profile-image/{key}/{image_file.name}'
+            
+            User.objects.filter(user_code=user.user_code).update(nickname=nickname, profile_image_url=url)
+            
+            return JsonResponse({'message' : 'SUCCESS'}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+    @login_decorator
+    def get(self, request):
+        try:
+            user = request.user
+        
+            user_profile = {
+                "nickname"          : user.nickname,
+                "profile_image_url" : user.profile_image_url if user.profile_image_url else DEFAULT_IMAGE_URL
+            }
+
+            return JsonResponse({'message' : 'SUCCESS', 'results' : user_profile}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+            
