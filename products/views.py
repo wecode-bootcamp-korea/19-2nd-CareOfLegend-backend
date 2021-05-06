2eaf11af1b711853463bc202afa9b70c7b2d0db2
ahnjeonghyun <tgrf3090@gmail.com>
2021-05-06 16:31:23 +0900

    [19기 안정현]Conflict 해결

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 import my_settings
 
-from django.http     import JsonResponse
-from django.views    import View
+from django.http       import JsonResponse
+from django.views      import View
+from django.db.models  import Q
 
-from products.models import MainCategory, SubCategory, HealthGoal
+from products.models   import (MainCategory, SubCategory, Product,
+                               ProductImage, HealthGoal, ProductHealthGoal,
+                               Symbol, ProductSymbol)
 
 class CategoryListView(View):
     def get(self,request):
@@ -19,7 +22,6 @@ class CategoryListView(View):
                  
 
         return JsonResponse({'result':result}, status=200) 
-
 
 class ProductCategoryView(View):
     def get(self, request):
@@ -49,6 +51,52 @@ class HealthGoalView(View):
                 'icon_url'       : health_goal.icon_url
                 } for health_goal in health_goals]
 
+        return JsonResponse({'result' : result}, status = 200)
+
+class ProductListView(View):
+    def get(self,request):
+        sub_categories  = request.GET.get('sub_category',None)
+        health_goals    = request.GET.get('health_goal',None)
+        is_new          = request.GET.get('is_new',None)
+        
+        if health_goals:
+            health_goals_products = ProductHealthGoal.objects.filter(health_goal_id = health_goals)
+            result=[{
+                    'product_id'             : product.product.id,
+                    'product_health_goal_id' : product.health_goal_id,
+                    'product_name'           : product.product.name,
+                    'product_sub_name'       : product.product.sub_name,
+                    'product_price'          : product.product.price,
+                    'product_is_new'         : product.product.is_new,
+                    'product_description'    : product.product.description.split(','),
+                    'product_image'          : product.product.productimage_set.filter(thumbnail_status=1).first().image_url if product.product.productimage_set.filter(thumbnail_status=1).first() else None
+                    } for product in health_goals_products]
+
+            return JsonResponse({'result' : result}, status=200)
+        q = Q()
+
+        if not sub_categories and not health_goals and not is_new:
+            products = Product.objects.all()
+                        
+        if sub_categories:
+            q &= Q(sub_category = sub_categories)
+                        
+        if is_new:
+            q &= Q(is_new = is_new)
+                        
+        products = Product.objects.filter(q)
+
+        result=[{
+            'product_id'              : product.id,
+            'product_name'            : product.name,
+            'product_sub_category_id' : product.sub_category_id,
+            'product_sub_name'        : product.sub_name,
+            'product_price'           : float(product.price),
+            'product_is_new'          : product.is_new,
+            'product_description'     : product.description.split(','),
+            'product_tumbnail_image'  : product.productimage_set.get(thumbnail_status = True).image_url if product.productimage_set.filter(thumbnail_status=True).first() else None
+                    } for product in products]
+        
         return JsonResponse({'result' : result}, status = 200)
 
 
